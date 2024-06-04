397e850136c87bb2f46bb366d5149a0c80100157
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2024-06-04 19:58:47 +0200

    add missing type-hint

--- src/customer.py
+++ src/customer.py
@@ -1,5 +1,5 @@
 class Customer:
-    def __init__(self, id: int, name: str, email: str, billing: float, location):
+    def __init__(self, id: int, name: str, email: str, billing: float, location: str):
         self.id = id
         self.name = name
         self.email = email
